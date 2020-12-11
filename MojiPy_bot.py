4486d001ba788c468d69b3f108ca2be053f10c30
Red Ants Team <63103476+RedAntsSec@users.noreply.github.com>
2020-12-11 16:40:22 +0000

    Add files via upload

---
+++ MojiPy_bot.py
@@ -0,0 +1,57 @@
+# MojiPy bot, written by Darcy and Mr Nobody. @RedAntsS3c(Telegram), github.com/RedAntsSec
+
+#Imports
+from pyrogram import Client , filters
+import requests
+import json
+import Mojinterpreter
+
+#pyrogram api
+# You should enter your API details here:
+api_id = 0
+api_hash = ""
+name = ""
+
+#App
+app = Client(name , api_id = api_id , api_hash = api_hash)
+
+#Rextester
+def rextester(code):	
+    data = {
+        "LanguageChoice":"5",
+        "Program":code,
+        "Input":"",
+        "CompileArgs":""
+        }
+    req = requests.post(url="https://rextester.com/rundotnet/api" , data=data)
+    result = json.loads(req.text)
+    if result["Errors"] != None and result["Warnings"] != None:
+        result = "Results: \n" + str(result["Result"]) + "\nWarnings: \n" + str(result["Warnings"]) + "Errors: \n" + str(result["Errors"])
+    elif result["Errors"] != None and result["Warnings"] == None:
+        result = "Results: \n" + str(result["Result"]) + "\nErrors: \n" + str(result["Errors"])
+    elif result["Errors"] == None and result["Warnings"] != None:
+        result = "Results: \n" + str(result["Result"]) + "\nWarnings: \n" + str(result["Warnings"])
+    else:
+        result = "Results: \n" + str(result["Result"])
+    
+    return result
+
+#python to moji
+@app.on_message((filters.group | filters.private) & filters.regex("^[Dd]moji$") , group = 1)
+def Moji(client , message) :
+	textl = Mojinterpreter.dinterpret(message.reply_to_message.text)
+	app.send_message(message.chat.id , textl)
+
+#moji to python
+@app.on_message((filters.group | filters.private) & filters.regex("^[Mm]oji$") , group = 1)
+def Moji(client , message) :
+	textl = Mojinterpreter.interpret(message.reply_to_message.text)
+        if textl == "NotMoji":
+            app.send_message(message.chat.id , "There is not any moji character in the given code!")
+        else:
+            result = rextester(textl)
+	    app.send_message(message.chat.id , result)
+	
+
+#Run
+app.run()
---
+++ Mojinterpreter.py
@@ -0,0 +1,96 @@
+# MojiPy program. Written by Darcy from @RedAntsSec(Github), @RedAntsS3c(Telegram)
+
+# Emojies dictionary
+keys = {
+        "🖨": "print",
+        "❗️": "(",
+        "❕": ")",
+        "⛓": "\"",
+        "🌀": "for",
+        "⏩": ">",
+        "⏪": "<",
+        "⏭": ">=",
+        "⏮": "<=",
+        "🍆": "in",
+        "🕯": "while",
+        "🧨": "break",
+        "💣": "exit()",
+        "⚖️" : "if",
+        "💤": "sleep",
+        "❌": "not",
+        "⛔️": "!",
+        "✅": "=",
+        "❇️" : "is",
+        "➡️" : "elif",
+        "🛑": "else",
+        "🇮🇷": "def",
+        "🛂": "pass",
+        "🚶‍♂️": "return",
+        "📈": "range",
+        "💁‍♂️": "try",
+        "🤦‍♂️": "except",
+        "😫": "and",
+        "😮": "or",
+        "🤬": "raise",
+        "👫": "with",
+        "🤥": "False",
+        "🤐": "True",
+        "👽": "global",
+        "👩‍🏫": "class"
+        }
+
+# Converts python keywords to emojies.
+def dinterpret(string):
+    string = string.strip()
+    result = ""
+    x = 0
+
+    if "\'" in string:
+        x += 1
+        ans = string.split("\'")
+        for part in ans:
+            index = ans.index(part)
+            
+            if index % 2 == 0:
+                for keychar, valchar in keys.items():
+                    part = part.replace(valchar,keychar)
+                result += part 
+            else:
+                result += "⛓" + part + "⛓" 
+    
+    if '\"' in string:
+        x += 1
+        ans = string.split("\"")
+        for part in ans:
+            if ans.index(part) % 2 == 0:
+                for keychar, valchar in keys.items():
+                    part = part.replace(valchar,keychar)
+                result += part
+            else:
+                result += "⛓" + part + "⛓"
+    if x == 0:
+        for keychar, valchar in keys.items():
+            string = string.replace(valchar,keychar)
+        return string
+    else:
+        return result
+
+# Converts emojies to python keywords
+def interpret(string):
+    string = string.strip()
+    x = 0
+    for char in keys.keys():
+        if char in string:
+            x = 1
+            break
+    for count in range(65,91):
+        if chr(count) in string:
+            x += 1
+            break
+
+    if x != 2:
+        return "NotMoji"
+    for keychar , valchar in keys.items():
+        string = string.replace(keychar , valchar)
+    return string
+
